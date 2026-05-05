c1ea82bf72fccf8318dda60bf3c84f9a4b45f32c
zhura24 <anangdirga01@gmail.com>
2026-05-06 04:32:53 +0700

    fix login jwt final

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -20,7 +20,10 @@ export async function POST(req: NextRequest) {
     );
 
     if (rows.length === 0) {
-      return Response.json({ message: "User tidak ditemukan" }, { status: 404 });
+      return Response.json(
+        { message: "User tidak ditemukan" },
+        { status: 404 }
+      );
     }
 
     const user = rows[0];
@@ -28,7 +31,15 @@ export async function POST(req: NextRequest) {
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return Response.json({ message: "Password salah" }, { status: 401 });
+      return Response.json(
+        { message: "Password salah" },
+        { status: 401 }
+      );
+    }
+
+    
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET belum diset di environment");
     }
 
     const token = jwt.sign(
@@ -37,7 +48,7 @@ export async function POST(req: NextRequest) {
         username: user.username,
         role: user.role,
       },
-      process.env.JWT_SECRET,
+      process.env.JWT_SECRET!, 
       { expiresIn: "1d" }
     );
 
@@ -51,6 +62,12 @@ export async function POST(req: NextRequest) {
       },
     });
 
+  } catch (error: any) {
+    console.error("LOGIN ERROR:", error);
+    return Response.json(
+      { message: "Terjadi kesalahan server" },
+      { status: 500 }
+    );
   } finally {
     conn.release();
   }
